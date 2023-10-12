6dbbdffe5f3c3937d7e767681acfff0de6046635
Ubuntu <ubuntu@ip-172-31-23-223.ec2.internal>
2023-10-12 22:12:45 +0000

    Added hover over functionality, enabled clicking on suggestions. Major Styling Changes.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
+const searchCont = document.querySelector(".search-container");
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
@@ -27,6 +28,17 @@ function searchHandler(e)
 	//Search Results
 
 	let matchedFruit = search(fruitInput);
+
+	//Toggle Search CSS for background
+
+	if(matchedFruit.length == 0)
+	{
+		searchCont.classList.remove("search-background");
+	}
+	if(matchedFruit.length > 0 && !searchCont.classList.contains("search-background"))
+	{
+		searchCont.classList.add("search-background");
+	}
 
 	//Sort Results by Length and Alphabet
 
@@ -61,7 +73,14 @@ function showSuggestions(results, inputVal) {
 		
 		suggElmt.innerText = n;
 
+		suggElmt.addEventListener("dbclick",useSuggestion);
+
+		suggElmt.addEventListener("mouseover",toggleHighlightSuggestion);
+		suggElmt.addEventListener("mouseout",toggleHighlightSuggestion);
+
 		suggestions.appendChild(suggElmt);
+
+
 
 	});
 
@@ -79,7 +98,14 @@ Add an event listener which triggers when a user clicks. Write a function useSug
 populate the Search Bar with the suggestion. Add this function to the event listener.
 */
 function useSuggestion(e) {
-	// TODO
+
+	console.log('You like the suggestion ' + e.target.outerText);
+	console.log(console.dir(e));
+
+	document.querySelector("input#fruit").value = e.target.outerText;
+
+	searchHandler();
+	
 }
 
 /*
@@ -88,6 +114,13 @@ Add an event listener to trigger whenever a user hovers over one of the suggesti
 Write a function which highlights the suggestion. Attach this function to the event listener.
 ---------
 */
+
+function toggleHighlightSuggestion(e) 
+{
+	e.target.classList.toggle("highlighted");
+}
+
+
 
 
 
